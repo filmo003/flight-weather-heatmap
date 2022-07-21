d4d373f69036ebd00ffc55191c442e63d8ace4e6
MikeEckels <32800922+MikeEckels@users.noreply.github.com>
2022-07-21 17:45:37 -0400

    stuff

--- API/APITest.py
+++ API/APITest.py
@@ -1,21 +1,25 @@
 import pandas
 from backendAPI import API
 
-csv = "./data/output.csv"
+csv = "./data/example_data/example_data.csv"
 #csv = "./data/monthly_f35_weather_canx.csv"
 #csv = "./data/monthly_global_hawk_weather_canx.csv"
 
 def main():
-    api = API(csv, useMemory = False, deleteExisting = True)
+    api = API(csv, useMemory = False, deleteExisting = False)
     connection = api.createDatabase()
 
-    #This is how you cet Canx data for a specific base. Returns {Name, Month, Canx}
-    #canx = api.getCanx("name", "global-Hawk")
+    #This is how you get Canx data for a specific base and aircraft. Returns {Name, Month, Canx}
+    canx = api.getCanx("BEALE AFB", "global-Hawk")
     #for canx in canx: print(canx)
 
+    #This is how you get DEV Canx data for a specific base. Returns {Name, Month, Canx}
+    # canx = api.getDEVCanx("BEALE AFB")
+    # for canx in canx: print(canx)
+
     #This is how you can query all base info. Returns {Name, Latitude, Longitude}
-    # bases = api.getBases()
-    # for base in bases: print(base)
+    #bases = api.getBases()
+    #for base in bases: print(bases)
 
     #This is how you can create your own custom query. Returns all results
     # custom = api.executeQuery("SELECT * FROM weatherData")
