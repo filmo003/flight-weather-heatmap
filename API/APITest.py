2f4ca71292daa3dc8751baef867baef5e219f003
MikeEckels <32800922+MikeEckels@users.noreply.github.com>
2022-07-21 12:31:20 -0400

    Added canx method and custom query

--- API/APITest.py
+++ API/APITest.py
@@ -5,10 +5,21 @@ csv = "./data/monthly_f35_weather_canx.csv"
 #csv = "./data/monthly_global_hawk_weather_canx.csv"
 
 def main():
-    api = API(csv, useMemory = True, deleteExisting = True)
+    api = API(csv, useMemory = False, deleteExisting = False)
     connection = api.createDatabase()
-    bases = api.getBases()
-    for base in bases: print(base)
+
+    #This is how you cet Canx data for a specific base. Returns {Name, Month, Canx}
+    canx = api.getCanx("EGLIN AFB")
+    for canx in canx: print(canx)
+
+    #This is how you can query all base info. Returns {Name, Latitude, Longitude}
+    # bases = api.getBases()
+    # for base in bases: print(base)
+
+    #This is how you can create your own custom query. Returns all results
+    # custom = api.executeQuery("SELECT * FROM weatherData")
+    # print(custom)
+
     connection.close()
 
 if __name__ == "__main__":
