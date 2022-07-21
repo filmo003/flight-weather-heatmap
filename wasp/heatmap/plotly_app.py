04ee66b5ebca28bf5dfa2c3deeb6b0980bbcebb2
Andrej4156 <64599107+Andrej4156@users.noreply.github.com>
2022-07-21 07:48:50 -0400

    Before Backend Update

--- wasp/heatmap/plotly_app.py
+++ wasp/heatmap/plotly_app.py
@@ -2,7 +2,7 @@ import dash
 # from dash import dcc, html
 import dash_core_components as dcc
 import dash_html_components as html
-
+import os
 
 from django_plotly_dash import DjangoDash
 
@@ -10,31 +10,27 @@ app = DjangoDash('SimpleExample')   # replaces dash.Dash
 
 import pandas as pd
 quakes = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv')
+df = pd.read_csv(os.getcwd() + "\\heatmap\\mockdata.csv")
 
 import plotly.graph_objects as go
-fig = go.Figure(go.Densitymapbox(lat=quakes.Latitude, lon=quakes.Longitude, z=quakes.Magnitude,
-                                 radius=10))
-fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
 
+fig = go.Figure(go.Densitymapbox(lat=df['latitude'], lon=df['longitude'], z=df['cancelPercent'], radius=20, text=df['base']))
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 app.layout = html.Div([
     dcc.Graph(
-        id='dropdown-color',
-        style={"width": "50%"},
-
+        id='heatmap',
+        style={"width":"50%", "height": "700px"},
+        figure=fig
     ),
-
-
 ])
 
 @app.callback(
-    dash.dependencies.Output('output-color', 'children'),)
+    dash.dependencies.Output('heatmap', 'figure'),)
 def callback_color():
     fig = go.Figure(go.Densitymapbox(lat=quakes.Latitude, lon=quakes.Longitude, z=quakes.Magnitude,
                                      radius=10))
     fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    fig.show()
     return fig
