0d20cdf31d883b85deb77aaa832acd6c1e243226
filmo003 <collin.gilmore47@gmail.com>
2022-07-21 19:28:57 -0400

    test: :white_check_mark: add smoke test to see if heatmap loads

--- wasp/heatmap/tests.py
+++ wasp/heatmap/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.test import Client
 
 # Create your tests here.
-def test_heatmap(self):
-  response = self.client.get('/heatmap')
-  self.assertEqual(response.status_code, 200)
+
+class smokeTests(TestCase):
+  def test_heatmap(self):
+    c = Client()
+    response = c.get('/heatmap/')
+    self.assertEqual(response.status_code, 200)
